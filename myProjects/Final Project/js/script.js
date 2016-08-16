60513a2c4e86fe68bc4cd9423f71c8afa7a792a2
Matthew Carpenter <Matthew.j.carpenter1@Gmail.com>
2016-08-16 15:45:01 -0400

    replaced project

--- myProjects/Final Project/js/script.js
+++ myProjects/Final Project/js/script.js
@@ -1,30 +1,25 @@
 
 
 $(document).ready(function(){
-    $(document).on('scroll', hideBar);
-    $(document).on('scroll', fixNav);
+    $(document).on('scroll', toggleSize);
+
+
 });
-
-function hideBar(){
-
-    var speed = 700
-    if ($(window).scrollTop() < 60) {
-    $('.nav').slideUp(speed);
-    $('#myName').css('display', 'unset');
     
-}else {
-    $('.nav').slideDown(speed);
-    $('#myName').addClass('slideIn');
-    $('#myName').css('display', 'none');
-}
-};
-
-function fixNav(){
-
-    if ($(window).scrollTop() > 750){
-        $('.nav').css('background-attachment', 'fixed');
-        $('.about').css('background-attachment', 'fixed');
-    } else{
-
+function toggleSize(){
+    if ($(window).scrollTop() > 10){
+        $('.main-header').animate( { height:"70px" }, { duration:0 });
+        $('#title').slideUp(0);
+        $('#name').css('display', 'unset');
+        $('#name').fadeIn(100);
+       
     }
-}
+    else{
+        $('.main-header').animate( { height:"450px" }, { duration:0 });
+         $('#name').fadeOut(100);
+         $('#name').css('display', 'none');
+         $('#title').fadeIn(400);
+         $('#title').slideDown(300);
+    }
+  
+};
